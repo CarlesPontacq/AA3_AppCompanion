483bd4f5781efec75c2995a40ba0d432632f04fd
JoanaMorenoC <juana.moreno@enti.cat>
2025-11-24 11:03:21 +0100

    Start adding bottom navigation bar

--- app/src/main/java/com/example/appcompanion/MainActivity.kt
+++ app/src/main/java/com/example/appcompanion/MainActivity.kt
@@ -5,16 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        bottomNavigationView = findViewById(R.id.navbar)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            handleNavigationItemSelected(item.itemId)
+        }
+    }
+
+    private fun handleNavigationItemSelected(itemId: Int): Boolean {
+        return when (itemId) {
+            R.id.cards -> {
+
+                true
+            }
+            R.id.deck -> {
+
+                true
+            }
+            R.id.chat -> {
+
+                true
+            }
+            R.id.profile -> {
+
+                true
+            }
+            else -> false
         }
     }
 }
