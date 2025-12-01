6e2424b342fe6f13b2bf5f87ee32dab0161d1563
CarlesPontacq <carles.pontacq@estudiant.enti.cat>
2025-12-01 09:56:12 +0100

    Splash screen functionality made

--- app/src/main/java/com/example/appcompanion/MainActivity.kt
+++ app/src/main/java/com/example/appcompanion/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.appcompanion
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var splashScreenLayout: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        splashScreenLayout = findViewById(R.id.splashLayout)
+        splashScreenLayout.setOnClickListener{onButtonClick()}
+    }
+
+    private fun onButtonClick(){
+        val intent = Intent(this, LoginActivity::class.java);
+        startActivity(intent)
     }
 }
