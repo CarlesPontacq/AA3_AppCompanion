0820e15a4c77c0c595d4f8608f8bdeacf1838952
JoanaMorenoC <juana.moreno@estudiant.enti.cat>
2025-12-02 11:03:18 +0100

    Readd splash screen

---
+++ app/src/main/java/com/example/appcompanion/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.appcompanion
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var splashScreenLayout: LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        splashScreenLayout = findViewById(R.id.splashLayout)
+        splashScreenLayout.setOnClickListener{onButtonClick()}
+    }
+
+    private fun onButtonClick(){
+        val intent = Intent(this, LoginActivity::class.java);
+        startActivity(intent)
+    }
+}
