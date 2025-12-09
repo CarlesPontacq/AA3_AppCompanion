970e5fb5f72b49f083bb9c6b8854fcffbc64b02a
JoanaMorenoC <juana.moreno@estudiant.enti.cat>
2025-12-09 13:26:39 +0100

    Start adding top toolbar

--- app/src/main/java/com/example/appcompanion/DeckFragment.kt
+++ app/src/main/java/com/example/appcompanion/DeckFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 
 class DeckFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +14,10 @@ class DeckFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_deck, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as? AppCompatActivity)?.supportActionBar?.title = "Deck"
     }
 }
--- app/src/main/java/com/example/appcompanion/MainActivity.kt
+++ app/src/main/java/com/example/appcompanion/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
@@ -13,24 +14,32 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var toolbarView: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Find navbar and set listener to change fragment when tapped
         bottomNavigationView = findViewById(R.id.navbar)
-
         bottomNavigationView.setOnItemSelectedListener { item ->
             handleNavigationItemSelected(item.itemId)
         }
 
+        // Find custom toolbar and set it as action bar
+        toolbarView = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbarView)
+
+        // Open fragment for Cards, the default screen
         loadFragment(CardsFragment())
     }
 
+    // Replace current fragment with the specified one
     private fun loadFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(R.id.frame, fragment).commit()
     }
 
+    // Handle navbar item being tapped (change current screen)
     private fun handleNavigationItemSelected(itemId: Int): Boolean {
         return when (itemId) {
             R.id.cards -> {
