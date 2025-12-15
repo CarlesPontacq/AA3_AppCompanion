b30c44cbe7fbbc19ec826a0c95dd5814e7e5b145
CarlesPontacq <carles.pontacq@enti.cat>
2025-12-15 10:10:50 +0100

    Api functional

--- app/src/main/java/com/example/appcompanion/CardsFragment.kt
+++ app/src/main/java/com/example/appcompanion/CardsFragment.kt
@@ -1,29 +1,65 @@
 package com.example.appcompanion
 
+import PokemonApi.PokemonApiCall
+import PokemonApi.PokemonApiInstance
+import PokemonApi.PokemonCardResponse
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.util.concurrent.TimeUnit
 
 class CardsFragment : Fragment() {
-    private val privateKey = "68b5881e-be78-4765-8037-c4ca1e74af1f" //<- Key de Pokemon TCG API
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        apiConnection();
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cards, container, false)
     }
 
-    private fun md5(input: String): String{
-        val md = MessageDigest.getInstance("MD5")
-        return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+    private fun apiConnection(){
+        val call = PokemonApiCall.apiService.searchCards("name:pikachu")
+
+        call.enqueue(object : Callback<PokemonCardResponse>{
+            override fun onResponse(
+                call: Call<PokemonCardResponse>,
+                response: Response<PokemonCardResponse>
+            ) {
+                if(response.isSuccessful){
+                    val cards = response.body()?.data ?: emptyList()
+
+                    cards.forEach{ card ->
+                        Log.d(
+                            "PokemonCard", "Name: ${card.name}, Types: ${card.types}"
+                        )
+                    }
+                }
+                else{
+                    Log.e(
+                        "PokemonCard",
+                        "Response error: ${response.code()} - ${response.message()}"
+                    )
+                }
+            }
+            override fun onFailure(call: Call<PokemonCardResponse>, t: Throwable) {
+                Log.e("ApiError", t.message ?: "Unknown error")
+            }
+        })
     }
 }
